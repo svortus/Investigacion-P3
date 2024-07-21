3a91d71651559f5889702b7abfddbed0a00457e2
cristhianchimbo50 <cristhian.chimbo@espoch.edu.ec>
2024-07-21 17:16:39 -0500

    se agrego el contenido

---
+++ js/nuevasFunc.js
@@ -0,0 +1,53 @@
+function openmodGagne() {
+    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
+    var width = 1200;
+    var height = 675;
+    var left = (window.innerWidth / 2) - (width / 2);
+    var top = (window.innerHeight / 2) - (height / 2);
+    window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
+}
+
+function openmodBrig() {
+    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
+    var width = 1200;
+    var height = 675;
+    var left = (window.innerWidth / 2) - (width / 2);
+    var top = (window.innerHeight / 2) - (height / 2);
+    window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
+}
+
+function openmodAssur() {
+    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
+    var width = 1200;
+    var height = 675;
+    var left = (window.innerWidth / 2) - (width / 2);
+    var top = (window.innerHeight / 2) - (height / 2);
+    window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
+}
+
+function openmodJona() {
+    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
+    var width = 1200;
+    var height = 675;
+    var left = (window.innerWidth / 2) - (width / 2);
+    var top = (window.innerHeight / 2) - (height / 2);
+    window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
+}
+
+function openmodDerickCarey() {
+    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
+    var width = 1200;
+    var height = 675;
+    var left = (window.innerWidth / 2) - (width / 2);
+    var top = (window.innerHeight / 2) - (height / 2);
+    window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
+}
+
+function openmodAddie() {
+    var url = 'https://view.genially.com/669c5cd39c64fa0477cad8fa';
+    var width = 1200;
+    var height = 675;
+    var left = (window.innerWidth / 2) - (width / 2);
+    var top = (window.innerHeight / 2) - (height / 2);
+    window.open(url, 'popup', 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',scrollbars=yes');
+}
